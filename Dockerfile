10c09e21b3424127d10ffaced85e1efd03d1ce98
余小波 <yuxiaobo64@gmail.com>
2022-05-19 01:01:08 +0800

    fix 改了一些东西

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,20 @@ WORKDIR /root/
 
 # DNS
 COPY ./config/resolv.conf /etc/resolv.conf
+# COPY ./config/Centos-7.repo /etc/yum.repos.d/CentOS-Base.repo
+
+# 必要的依赖
+# RUN yum -y install wget gcc gcc-c++ pcre pcre-devel zlib zlib-devel openssl openssl-devel autoconf libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers gd gd2 gd-devel gd2-devel perl-CPAN pcre-devel
 
 # 安装包
 COPY ./php /root/php-source
 COPY ./nginx /root/nginx-source
-
+# RUN cd ~/nginx-source && tar -zxvf ./nginx-1.20.2.tar.gz && cd nginx-1.20.2 && ./configure --prefix=/usr/local/nginx && make -j 4 && make install && echo "export PATH=$PATH:/usr/local/nginx/sbin" >> /etc/profile.d/nginx.sh && source /etc/profile.d/nginx.sh 
 # NGINX || php73
-RUN cd ~/nginx-source && chmod +x ./install.sh && sh ./install.sh && cd ~/php-source && chmod +x ./install.sh && ./install.sh && yum clean all && rm -rf ~/nginx-source &&  rm -rf ~/php-source
+RUN cd ~/nginx-source && chmod +x ./install.sh && sh ./install.sh && rm -rf ~/nginx-source
+RUN cd ~/php-source && chmod +x ./install.sh && ./install.sh && rm -rf ~/php-source
+RUN yum install -y certbot && python3-certbot-nginx
+RUN yum clean all
 
 VOLUME [ "/data" ]
 EXPOSE 80 443
--- php/install.sh
+++ php/install.sh
@@ -4,7 +4,7 @@
 yum -y install wget gcc gcc-c++ autoconf libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers gd gd2 gd-devel gd2-devel perl-CPAN pcre-devel
 
 # cmake
-cd ~/php-source && tar -zxvf cmake-3.13.2.tar.gz && cd cmake-3.13.2 && ./bootstrap && make && make install
+cd ~/php-source && tar -zxvf cmake-3.13.2.tar.gz && cd cmake-3.13.2 && ./bootstrap && make -j 8 && make install
 
 # libzip
 cd ~/php-source && tar -zxf libzip-1.5.2.tar.gz && cd libzip-1.5.2 && mkdir build && cd build && cmake .. && make -j4 && make test && make install
@@ -60,15 +60,10 @@ cd ~/php-source && tar -zxvf php-7.3.31.tar.gz && cd php-7.3.31  && ./configure 
     --with-fpm-group=nginx \
     --without-gdbm \
     --enable-fast-install \
-    --enable-fileinfo \
+    --disable-fileinfo \
     && make && make install \
     && cp php.ini-production /usr/local/php73/etc/php.ini \
     && sed -i 's#;extension_dir = "./"#extension_dir = "/usr/local/php73/lib/php/extensions/no-debug-non-zts-20180731"#g' /usr/local/php73/etc/php.ini \
     && sed -i 's#;date.timezone =#date.timezone = PRC #g' /usr/local/php73/etc/php.ini \
-    && echo "export PATH=$PATH:/usr/local/php73/bin/:/usr/local/php73/sbin/" >> /etc/profile.d/php.sh \
+    && echo export PATH=$PATH:/usr/local/php73/bin/:/usr/local/php73/sbin/ >> /etc/profile.d/php.sh \
     && source /etc/profile.d/php.sh 
-
-
-# certbot
-yum install -y certbot && python3-certbot-nginx
-
